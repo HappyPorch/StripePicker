d4801d95ea7dbe6e93393afef6a1bb697c9b5cc9
Filipe Sousa <filipe@endzone.io>
2018-10-17 17:06:30 +0100

    Fixed to only call getProducts once.

--- StripePicker/App_Plugins/StripePicker/stripepicker.resources.js
+++ StripePicker/App_Plugins/StripePicker/stripepicker.resources.js
@@ -1,60 +1,66 @@
 ﻿angular.module("umbraco.resources").factory("stripeResources", function ($http) {
     var stripeService = {};
     stripeService.products = {};
+    stripeService.products.ready = false;
+    stripeService.products.allProducts = [];
+
     stripeService.plans = {};
+    stripeService.plans.ready = false;
+    stripeService.plans.allPlans = [];
 
-    stripeService.fetchProducts = function () {
-        return $http.get("/umbraco/backoffice/StripePickerPlugin/StripePicker/GetProducts");
-    };
+    stripeService.fetchProducts = $http.get("/umbraco/backoffice/StripePickerPlugin/StripePicker/GetProducts");
 
-    stripeService.fetchPlans = function () {
-        return $http.get("/umbraco/backoffice/StripePickerPlugin/StripePicker/GetPlans");
-    };
+    stripeService.fetchPlans = $http.get("/umbraco/backoffice/StripePickerPlugin/StripePicker/GetPlans");
 
-    stripeService.getProducts = function () {
+    async function getProducts() {
         if (stripeService.products.ready) return stripeService.products;
-        stripeService.products.ready = false;
-        stripeService.products.allProducts = [];
 
-        stripeService.fetchProducts().then(function (res) {
+        var res;
+        try {
+            res = await stripeService.fetchProducts;
             var data = res.data;
             for (var product in data) {
                 if (data.hasOwnProperty(product)) {
                     stripeService.products.allProducts.push(data[product]);
                 }
             }
-        },
-            function (res) {
-                stripeService.products.error = res.data;
-            }
-        );
+        }
+        catch (error) {
+            stripeService.products.error = error;
+        }
+
         stripeService.products.ready = true;
         return stripeService.products;
+    }
+    stripeService.getProductsCache = getProducts();
+
+    stripeService.getProducts = async () => {
+        return stripeService.getProductsCache;
     };
 
-    stripeService.getProducts();
-    stripeService.getPlans = function () {
+    stripeService.getPlans = async () => {
         if (stripeService.plans.ready) return stripeService.plans;
 
-        stripeService.plans.ready = false;
-        stripeService.plans.allPlans = [];
-
-        stripeService.fetchPlans().then(function (res) {
+        var products = await stripeService.getProducts();
+        
+        var res;
+        try {
+            res = await stripeService.fetchPlans;
             var data = res.data;
             for (var plan in data) {
                 if (data.hasOwnProperty(plan)) {
-                    var productName = stripeService.getProducts().allProducts.filter(prod => {
+                    var productName = products.allProducts.filter(prod => {
                         return prod.Id === data[plan].ProductId
                     })[0].Name;
                     data[plan].FullName = productName + ": " + data[plan].Name;
                     stripeService.plans.allPlans.push(data[plan]);
                 }
             }
-        },
-            function (res) {
-                stripeService.plans.error = res.data;
-            }
-        );
+        }
+        catch (error) {
+            stripeService.plans.error = res.data;
+        }
+
         stripeService.plans.ready = true;
         return stripeService.plans;
     };
