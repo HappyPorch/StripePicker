f244ca7793c3ef46b6f98b98e558a908b20df465
Filipe Sousa <filipe@endzone.io>
2018-10-17 14:23:36 +0100

    Moved more code into resources.

--- StripePicker/App_Plugins/StripePicker/stripepicker.plan.controller.js
+++ StripePicker/App_Plugins/StripePicker/stripepicker.plan.controller.js
@@ -1,20 +1,3 @@
 ﻿angular.module("umbraco").controller("stripepicker.plan.controller", function ($scope, stripeResources) {
-
-    $scope.ready = false;
-    $scope.allPlans = [];
-
-    stripeResources.getPlans().then(function (res) {
-        var data = res.data;
-        for (var plan in data) {
-            if (data.hasOwnProperty(plan)) {
-                $scope.allPlans.push(data[plan]);
-            }
-        }
-        $scope.ready = true;
-    },
-        function (res) {
-            $scope.error = res.data;
-            $scope.ready = true;
-        }
-    );
+    $scope.data = stripeResources.getPlans();
 });
--- StripePicker/App_Plugins/StripePicker/stripepicker.resources.js
+++ StripePicker/App_Plugins/StripePicker/stripepicker.resources.js
@@ -1,14 +1,57 @@
 ﻿angular.module("umbraco.resources").factory("stripeResources", function ($http) {
     var stripeService = {};
+    stripeService.products = {};
+    stripeService.plans = {};
 
-    stripeService.getProducts = function () {
+    stripeService.fetchProducts = function () {
         return $http.get("/umbraco/backoffice/StripePickerPlugin/StripePicker/GetProducts");
     };
 
-    stripeService.getPlans = function () {
+    stripeService.fetchPlans = function () {
         return $http.get("/umbraco/backoffice/StripePickerPlugin/StripePicker/GetPlans");
     };
 
+    stripeService.getProducts = function () {
+        if (stripeService.products.ready) return stripeService.products;
+        stripeService.products.ready = false;
+        stripeService.products.allProducts = [];
+
+        stripeService.fetchProducts().then(function (res) {
+            var data = res.data;
+            for (var product in data) {
+                if (data.hasOwnProperty(product)) {
+                    stripeService.products.allProducts.push(data[product]);
+                }
+            }
+        },
+            function (res) {
+                stripeService.products.error = res.data;
+            }
+        );
+        stripeService.products.ready = true;
+        return stripeService.products;
+    };
+
+    stripeService.getPlans = function () {
+        if (stripeService.plans.ready) return stripeService.plans;
+        stripeService.plans.ready = false;
+        stripeService.plans.allPlans = [];
+
+        stripeService.fetchPlans().then(function (res) {
+            var data = res.data;
+            for (var plan in data) {
+                if (data.hasOwnProperty(plan)) {
+                    stripeService.plans.allPlans.push(data[plan]);
+                }
+            }
+        },
+            function (res) {
+                stripeService.plans.error = res.data;
+            }
+        );
+        stripeService.plans.ready = true;
+        return stripeService.plans;
+    };
     return stripeService;
 
 });
